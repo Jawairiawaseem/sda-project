2d1bf55db7c920531e6b16079f5270805d1c6794
Javeria <i221274@nu.edu.pk>
2024-11-23 17:54:40 +0500

    my work

--- src/application/sampleController.java
+++ src/application/sampleController.java
@@ -10,7 +10,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Optional;
-
+//elasdasdjaskdjlkasjdlkasjd
 public class sampleController
 {
 	@FXML
